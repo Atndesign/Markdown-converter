5b1c839061dc169f976e7692caf2367bc609f0d7
Kevin <Atnnetworkk@gmail.com>
2020-05-10 15:55:37 +0200

    Add result components dangerouslysethtml

--- src/components/Result.js
+++ src/components/Result.js
@@ -2,8 +2,20 @@ import React, { Component } from "react";
 
 class Result extends Component {
   state = {};
+
+  updateHtml = () => {
+    return { __html: this.props.result };
+  };
+  componentDidUpdate() {
+    this.updateHtml();
+  }
   render() {
-    return <div className="result"></div>;
+    return (
+      <div className="result">
+        <h2 className="markdown__title">Result</h2>
+        <div dangerouslySetInnerHTML={this.updateHtml()}></div>
+      </div>
+    );
   }
 }
 
