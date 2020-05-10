021abf39d24b431437cb05c77772c56cbc1cec4b
Kevin <Atnnetworkk@gmail.com>
2020-05-10 16:05:55 +0200

    Add functions, conversion download md files

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,17 +5,45 @@ import Result from "./Result";
 class Main extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      dlLink: "",
+    };
   }
+
+  handleMarkdownInput = (e) => {
+    this.setState({
+      markdown: e.target.value,
+      html: this.handleConversion(e.target.value),
+    });
+  };
+
+  handleConversion = (markdownValue) => {
+    let showdown = require("showdown");
+    let converter = new showdown.Converter();
+    return converter.makeHtml(markdownValue);
+  };
+
+  downloadFile = () => {
+    let fileBlob = new Blob([this.state.markdown], { type: "text" });
+    this.setState({
+      dlLink: window.URL.createObjectURL(fileBlob),
+    });
+  };
+
   render() {
     return (
       <section className="main">
         <div className="row">
           <div className="col-lg-6">
-            <MarkdownInput />
+            <MarkdownInput
+              handleMarkdownInput={this.handleMarkdownInput}
+              handleConversion={this.handleConversion}
+              downloadFile={this.downloadFile}
+              dlLink={this.state.dlLink}
+            />
           </div>
           <div className="col-lg-6">
-            <Result />
+            <Result result={this.state.html} />
           </div>
         </div>
       </section>
--- src/components/MarkdownInput.js
+++ src/components/MarkdownInput.js
@@ -5,14 +5,23 @@ class MarkdownInput extends Component {
   render() {
     return (
       <div className="markdown">
-        <div className="markdown__title"></div>
+        <h2 className="markdown__title">Markdown</h2>
         <textarea
-          name=""
+          onInput={(e) => this.props.handleMarkdownInput(e)}
           id=""
           cols="30"
           rows="10"
           className="markdown__text"
         ></textarea>
+        <input onChange={(e) => this.props.load()} type="file" name="" id="" />
+        <a
+          className="btn btn-primary"
+          href={this.props.dlLink}
+          download="Markdown.md"
+          onClick={(e) => this.props.downloadFile()}
+        >
+          Download MD file
+        </a>
       </div>
     );
   }
